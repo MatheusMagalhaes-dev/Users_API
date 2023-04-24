79f8ef441d90ef20274e58dbca028b8e6f0f0f7d
MatheusFelipeM <mahteu8@gmail.com>
2023-04-24 10:40:23 -0300

    add controllers and middlewares

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,89 @@
+import { Router } from "express";
+
+import { UserRepository } from "../repositories/user.repository";
+
+import { validate } from "@middlewares";
+
+import {
+    createUserSchema,
+    getUserSchema,
+    updateUserSchema,
+    updateUserPasswordSchema,
+    deleteUserSchema
+} from '@schemas';
+
+import { IUser } from "@ts";
+
+const UserController = Router();
+
+UserController.get('/', async (req, res) => {
+    try {
+        const users = await UserRepository.getUsers();
+
+        return res.send(users);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+UserController.get('/:id', validate(getUserSchema), async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await UserRepository.getUser(id);
+
+        return res.send(user);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+UserController.post('/', validate(createUserSchema), async (req, res) =>{
+    try {
+        const data: IUser = req.body;
+
+        const response = await UserRepository.createUser(data);
+
+        return res.send(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+UserController.put('/:id', validate(updateUserSchema), async (req,res) =>{
+    try {
+        const { id } = req.params;
+        const data: IUser = req.body;
+
+        const response = await UserRepository.updateUser(id, data);
+
+        return res.send(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+UserController.patch('/:id/password', validate(updateUserPasswordSchema), async (req, res) =>{
+    try {
+        const { id } = req.params;
+        const { password } : IUser = req.body;
+
+        const response = await UserRepository.updateUserPassword(id, password);
+
+        return res.send(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+UserController.delete('/:id', validate(deleteUserSchema), async (req, res) =>{
+    try {
+        const { id } = req.params;
+
+        await UserRepository.deleteUser(id);
+
+        return res.send();
+    } catch (error) {
+        console.error(error);
+    }
+});
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import { z } from 'zod';
 //GetUser
 //Nesse caso para descobrir os usuarios cadastrados
@@ -13,5 +14,27 @@ export const createUserSchema =z.object({
         name: z.string().min(3).max(50),
         email: z.string().email(),
         password: z.string().min(5).max(20),
+    })
+})
+
+export const updateUserSchema = z.object({
+    body: z.object({
+        name: z.string().min(3).max(50).optional(),
+        email: z.string().email().optional(),
+    })
+})
+
+export const updateUserPasswordSchema = z.object({
+    body: z.object({
+        password: z.string().min(6).max(20),
+    }),
+    params: z.object({
+        id: z.custom(isValidObjectId),
+    })
+})
+
+export const deleteUserSchema = z.object({
+    params: z.object({
+        id: z.custom(isValidObjectId),
     })
 })
